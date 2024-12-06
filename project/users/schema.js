d0dfd0640b87e0f67d1dc72751111241cf281882
christina <christinakang0628@gmail.com>
2024-12-06 02:31:13 -0500

    remote server

---
+++ project/users/schema.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    firstName: String,
+    email: String,
+    lastName: String,
+    role: {
+        type: String,
+        enum: [, "ADMIN", "USER", "ANONYMOUS"],
+        default: "USER",
+      },
+    },
+    { collection: "users"}
+);
+
