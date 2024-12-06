b97cfb38cde9d183d108b235f63418fa418085c8
christina <christinakang0628@gmail.com>
2024-12-06 03:01:59 -0500

    changed schema

--- project/users/schema.js
+++ project/users/schema.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    firstName: String,
-    email: String,
-    lastName: String,
+    name: String,
     role: {
         type: String,
         enum: [, "ADMIN", "USER", "ANONYMOUS"],
